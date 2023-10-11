829bbdd01812fde4ba0fe808bf4902850b3bc46f
Naoko Sugawara <infoseek122@gmail.com>
2023-10-11 12:49:36 -0400

    adding addNumbers

--- new_file.js
+++ new_file.js
@@ -36,4 +36,52 @@ class Clock {
 
 }
 
-const clock = new Clock();
+// const clock = new Clock();
+
+
+
+// ------------------------------------------------------------
+
+
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  // it's okay if this part is magic; it just says that we want to
+  // 1. output the prompt to the standard output (console)
+  // 2. read input from the standard input (again, console)
+
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function completionCallback(sum) {
+    // print total sum
+    console.log(`total sum = ${sum}`);
+
+    // close readline
+    reader.close();
+}
+
+
+function addNumbers(sum, numsLeft, completionCallback){
+
+    reader.question("What is number? : ", function (answer) {
+
+            const parsed = parseInt(answer);
+            sum += parsed;
+            console.log(`sum = ${sum}`);
+
+            numsLeft--;
+
+            if (numsLeft === 0) {
+                completionCallback(sum);
+            } else {
+                addNumbers(sum, numsLeft, completionCallback);
+            }
+      })
+
+}
+
+
+addNumbers(0, 3, completionCallback);
